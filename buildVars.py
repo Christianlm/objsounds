30990d75ddfa3e8058100a51baacc65a9f2bb921
Christianlm <llajta2012@gmail.com>
2020-06-19 10:42:30 +0200

    Imports Syntax for python3
    Using speech.getPropertiesSpeech medotd instead speech.getSpeechTextForProperties
    
    Importing scriptHandler
    
    Changed key command to disable objsounds Nvda+Shift+Delete.

---
+++ buildVars.py
@@ -0,0 +1,49 @@
+# -*- coding: UTF-8 -*-
+
+# Build customizations
+# Change this file instead of sconstruct or manifest files, whenever possible.
+
+# Full getext (please don't change)
+_ = lambda x : x
+
+# Add-on information variables
+addon_info = {
+	# for previously unpublished addons, please follow the community guidelines at:
+	# https://bitbucket.org/nvdaaddonteam/todo/raw/master/guideLines.txt
+	# add-on Name, internal for nvda
+	"addon_name" : "objsounds",
+	# Add-on summary, usually the user visible name of the addon.
+	# Translators: Summary for this add-on to be shown on installation and add-on information.
+	"addon_summary" : _("obj sounds"),
+	# Add-on description
+	# Translators: Long description to be shown for this add-on on add-on information from add-ons manager
+	"addon_description" : _("""Plays sounds based on objects as you move to them."""),
+	# version
+	"addon_version" : "20.06.17-dev",
+	# Author(s)
+	"addon_author" : u"Tyler Spivey",
+	# URL for the add-on documentation support
+	"addon_url" : None,
+	# Documentation file name
+	"addon_docFileName" : "readme.html",
+	# Minimum NVDA version supported (e.g. "2018.3.0")
+	"addon_minimumNVDAVersion" : "2019.1.0",
+	# Last NVDA version supported/tested (e.g. "2018.4.0", ideally more recent than minimum version)
+	"addon_lastTestedNVDAVersion" : "2020.2.0",
+	# Add-on update channel (default is stable or None)
+	"addon_updateChannel" : None,
+}
+
+
+import os.path
+
+# Define the python files that are the sources of your add-on.
+# You can use glob expressions here, they will be expanded.
+pythonSources = [os.path.join("addon", "*.py"), os.path.join("addon", "globalPlugins", "objsounds", "*.py")]
+
+# Files that contain strings for translation. Usually your python sources
+i18nSources = pythonSources + ["buildVars.py"]
+
+# Files that will be ignored when building the nvda-addon file
+# Paths are relative to the addon directory, not to the root directory of your addon sources.
+excludedFiles = []
