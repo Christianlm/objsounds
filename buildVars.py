4c6a069741111e9770c053d1b59512f74c6c0247
Christianlm <llajta2012@gmail.com>
2021-11-15 11:06:26 +0100

    compatibility for NVDA2022

--- buildVars.py
+++ buildVars.py
@@ -19,7 +19,7 @@ addon_info = {
 	# Translators: Long description to be shown for this add-on on add-on information from add-ons manager
 	"addon_description" : _("""Plays sounds based on objects as you move to them."""),
 	# version
-	"addon_version" : "21.08.14-dev",
+	"addon_version" : "21.11.14-dev",
 	# Author(s)
 	"addon_author" : u"Tyler Spivey",
 	# URL for the add-on documentation support
@@ -29,7 +29,7 @@ addon_info = {
 	# Minimum NVDA version supported (e.g. "2018.3.0")
 	"addon_minimumNVDAVersion" : "2019.1.0",
 	# Last NVDA version supported/tested (e.g. "2018.4.0", ideally more recent than minimum version)
-	"addon_lastTestedNVDAVersion" : "2021.1.0",
+	"addon_lastTestedNVDAVersion" : "2022.1.0",
 	# Add-on update channel (default is stable or None)
 	"addon_updateChannel" : None,
 }
